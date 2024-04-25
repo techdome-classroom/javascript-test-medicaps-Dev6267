2f4cdef0168b1bb704f0a5a5bc39bb6149047f8c
devendra_singh <rdev45450@gmail.com>
2024-04-25 13:28:59 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -5,3 +5,24 @@ function longestSubstring(s) {
 module.exports = { longestSubstring };
 
 
+function findLongestSubstringValue(str) {
+    let longestValue = 0;
+    let startValue = 0;
+    let seenValue = {};
+    
+    for (let end = 0; end < str.length; end++) {
+        if (seenValue[str[end]]) {
+            startValue = Math.max(startValue, seenValue[str[end]]);
+        }
+        
+        longestValue = Math.max(longestValue, end - startValue + 1);
+        seenValue[str[end]] = end + 1;
+    }
+    
+    return longestValue;
+}
+
+const input = "javascriptexamplecodes";
+const result = findLongestSubstringValue(input);
+console.log(result);
+The sliding window meth
